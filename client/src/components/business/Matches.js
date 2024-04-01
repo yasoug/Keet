f779fd316a28bdc76b13f5405d70196f094fb367
yasoug <ougouisyassine@gmail.com>
2024-04-01 06:55:28 -0700

    Matches update

--- client/src/components/business/Matches.js
+++ client/src/components/business/Matches.js
@@ -1,40 +1,39 @@
-import { Box } from '@mui/system'
-import { IconButton, Typography } from '@mui/material'
+import { Box, Typography, IconButton } from '@mui/material';
+import MatchCard from '../design/Card/MatchCard';
+import LikeIcon from '@mui/icons-material/Favorite';
+import DislikeIcon from '@mui/icons-material/Clear';
+import { useEffect, useState } from 'react';
+import Maybe from '../layout/Maybe';
 
-import MatchCard from '../design/Card/MatchCard'
-
-import LikeIcon from '@mui/icons-material/Favorite'
-import DislikeIcon from '@mui/icons-material/Clear'
-import { useEffect, useState } from 'react'
-import Maybe from '../layout/Maybe'
-
-const Matches = ({ onMatchClick = () => {}, onAttemptMatch = () => {}, users }) => {
-  const [_users, setUsers] = useState([])
-  const handleClick = () => {
-    onMatchClick(_users[0])
-  }
+const Matches = ({ onMatchClick = () => {console.log('clicked ')}, onAttemptMatch = () => {console.log('match')}, users }) => {
+  const [_users, setUsers] = useState([]);
 
   useEffect(() => {
-    setUsers(users)
-  }, [users])
+    setUsers(users);
+  }, [users]);
+
+  const handleClick = () => {
+    onMatchClick(_users[0]);
+  };
 
   const attemptMatching = () => {
-    const [potential, ...rest] = _users
-    onAttemptMatch(potential)
-    setUsers(rest)
-  }
+    const [potential, ...rest] = _users;
+    onAttemptMatch(potential);
+    setUsers(rest);
+  };
 
   const actions = [
-    <IconButton onClick={attemptMatching} key="dislike" aria-label="add to favorites">
+    <IconButton onClick={attemptMatching} key="dislike" aria-label="dislike">
       <DislikeIcon />
     </IconButton>,
-    <IconButton onClick={attemptMatching} key="like" aria-label="add to favorites">
+    <IconButton onClick={attemptMatching} key="like" aria-label="like">
       <LikeIcon />
     </IconButton>
-  ]
+  ];
 
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+    <div className='w-96'>
+           <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <Maybe
         condition={_users.length > 0}
         fallback={
@@ -47,10 +46,12 @@ const Matches = ({ onMatchClick = () => {}, onAttemptMatch = () => {}, users }) 
               borderRadius: 3,
               boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 12px'
             }}
-            variant="h5">
+            variant="h5"
+          >
             There are no more matches in your area
           </Typography>
-        }>
+        }
+      >
         <MatchCard
           data={_users[0]}
           onClick={handleClick}
@@ -60,7 +61,9 @@ const Matches = ({ onMatchClick = () => {}, onAttemptMatch = () => {}, users }) 
         />
       </Maybe>
     </Box>
-  )
-}
+    </div>
+    
+  );
+};
 
 export default Matches
